a69dcc5d84431adb6e3101409e90e28266eaaa6d
siIvin <garavito.silvia@gmail.com>
2020-05-08 20:45:21 -0600

    creacion de modelos de libro

---
+++ models/Books.js
@@ -0,0 +1,23 @@
+const moongose = require('mongoose');
+const Schema = moongose.Schema
+
+const bookSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    pages:{
+        type: Number,
+        required: true
+    },
+    is_active: {
+        type: Boolean,
+        default: true
+})
+
+const books = moongose.model('book', bookSchema);
+module.exports = books
