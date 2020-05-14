d2b6e8b07dd70a37bac811e2d9ea8373619676a3
siIvin <garavito.silvia@gmail.com>
2020-05-13 20:56:10 -0600

    agregar validacion a rutas de estudiantes

--- router/StudentRoutes.js
+++ router/StudentRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const StudentsController = require('../controller/StudentsController');
-//const {BookValidator}= require('../Validators')
+const {StudentValidator}= require('../Validators')
 
 
-router.post('/students', StudentsController.create);
+router.post('/students', StudentValidator.createOrUpdate, StudentsController.create);
 //Get all
 router.get('/students', StudentsController.find);
 //Read one
 router.get('/students/:id', StudentsController.findbyId);
-router.patch('/students/:id', StudentsController.findbyIdandUpdate);
+router.patch('/students/:id', StudentValidator.createOrUpdate, StudentsController.findbyIdandUpdate);
 router.delete('/students/:id', StudentsController.findbyIdAndDelete);
 
 module.exports = router
