40f42756e779b544a6c50fe76cebad7f3d43454b
siIvin <garavito.silvia@gmail.com>
2020-05-13 20:55:44 -0600

    agregar valicion a rutas de usarios

--- router/UserRoutes.js
+++ router/UserRoutes.js
@@ -4,12 +4,12 @@ const UsersController = require('../controller/UsersController');
 const {UserValidator}= require('../Validators');
 
 
-router.post('/users', UserValidator.create, UsersController.create);
+router.post('/users', UserValidator.createOrUpdate, UsersController.create);
 //Get all
 router.get('/users', UsersController.find);
 //Read one
 router.get('/users/:id', UsersController.findbyId);
-router.patch('/users/:id', UsersController.findbyIdandUpdate);
+router.patch('/users/:id', UserValidator.createOrUpdate, UsersController.findbyIdandUpdate);
 router.delete('/users/:id',UsersController.findbyIdAndDelete);
 
 module.exports = router
