89a569ce00d308f3a0c2669755b76d38ef4aa79c
siIvin <garavito.silvia@gmail.com>
2020-05-13 20:50:41 -0600

    cambio en validator de estudiantes

--- Validators/StudentValidator.js
+++ Validators/StudentValidator.js
@@ -6,6 +6,7 @@ module.exports={
             first_name: Joi.string().required(),
             last_name: Joi.string().required(),
             dateOfBirth: Joi.string().required(),
+            readBooks: Joi.array()
             //buscar cómo se hace una fecha en modelo
             //buscar como validar una referencia de un modelo de moongose
         })
