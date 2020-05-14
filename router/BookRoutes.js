0a18bd224095097f3b1712354687cc610bcbcb85
siIvin <garavito.silvia@gmail.com>
2020-05-13 20:10:59 -0600

    cambio de nombre del validator de libros

--- router/BookRoutes.js
+++ router/BookRoutes.js
@@ -4,12 +4,12 @@ const BooksController = require('../controller/BooksController');
 const {BookValidator}= require('../Validators')
 
 
-router.post('/books', BookValidator.create, BooksController.create);
+router.post('/books', BookValidator.createOrUPdate, BooksController.create);
 //Get all
 router.get('/books', BooksController.find);
 //Read one
 router.get('/books/:id', BooksController.findbyId);
-router.patch('/books/:id', BooksController.findbyIdandUpdate);
+router.patch('/books/:id', BookValidator.createOrUPdate, BooksController.findbyIdandUpdate);
 router.delete('/books/:id', BooksController.findbyIdAndDelete);
 
 module.exports = router
