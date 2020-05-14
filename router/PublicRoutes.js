e83430b53fb0bd8357e089b35ef17273d0ae8bd0
siIvin <garavito.silvia@gmail.com>
2020-05-13 20:05:37 -0600

    quitar los libros de rutas públicas

--- router/PublicRoutes.js
+++ router/PublicRoutes.js
@@ -2,6 +2,5 @@ const express= require('express');
 const router = express.Router();
 
 router.use(require('./AuthRoutes'));
-router.use(require('./BookRoutes'));//estas deben ser privadas
 
 module.exports = router
