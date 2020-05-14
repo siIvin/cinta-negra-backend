a99724ab79e4f976793c37396dbf48dbda759445
siIvin <garavito.silvia@gmail.com>
2020-05-13 19:51:33 -0600

    agregar validator para libros y estudiantes

--- Validators/index.js
+++ Validators/index.js
@@ -1,4 +1,8 @@
 const UserValidator= require('./UserValidator');
+const BookValidator= require('./BookValidator');
+const StudentValidator= require('./StudentValidator');
 module.exports={
     UserValidator,
+    BookValidator,
+    StudentValidator,
 }
