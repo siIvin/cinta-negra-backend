fd09c5a41ea7820c50258db358cc94d5b244442e
siIvin <garavito.silvia@gmail.com>
2020-05-15 17:00:00 -0600

    base de dato según entorno

--- database client/index.js
+++ database client/index.js
@@ -1,5 +1,6 @@
-const mongoose= require('mongoose')
-mongoose.connect(process.env.MONGO_URI,
+const mongoose= require('mongoose');
+const {MONGO_URI}= require('../config');
+mongoose.connect(MONGO_URI,
     { useNewUrlParser: true,
     useUnifiedTopology: true }
 )
